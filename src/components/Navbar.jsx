a64e106edc4888b93be30f449e92f7734f15aa32
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-05-20 13:06:24 -0600

    feat: add dropdown menu using state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,34 @@ import { IoMdMenu } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
 export default function NavBar() {
   const [menuClicked, setMenuClicked] = useState(false);
-
+  const navStyle = {
+    boxShadow: menuClicked ? 'none' : '1px 4px 5px rgb(216, 216, 216)'
+  }
   function clickHandler() {
     menuClicked ? setMenuClicked(false) : setMenuClicked(true);
   }
 
   const burgerMenu = menuClicked ? (
-    <IoMdClose className="BurgerMenu" size="30px" onClick={clickHandler} />
+    <IoMdClose className="burgerMenu" size="30px" onClick={clickHandler} />
   ) : (
-    <IoMdMenu className="BurgerMenu" size="30px" onClick={clickHandler} />
+    <IoMdMenu className="burgerMenu" size="30px" onClick={clickHandler} />
   );
   return (
     <>
-      <nav>
+      <nav style={navStyle}>
         <div>
-          <img className="logo-img" src={logo} alt="" />
+          <img className="logoImg" src={logo} alt="" />
         </div>
         <h1>Chicago Pizza</h1>
         {burgerMenu}
       </nav>
+      {menuClicked ? (<ul class="dropdownMenu visible">
+            <li>ITEM 1</li>
+            <li>ITEM 2</li>
+            <li>ITEM 3</li>
+            <li>ITEM 4</li>
+            <li>ITEM 5</li>
+        </ul>): (<></>)}
     </>
   );
 }
