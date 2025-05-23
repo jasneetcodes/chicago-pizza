f9da406123bb61b03b8a8b915c0f99631705b179
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-05-23 14:04:24 -0600

    feat: add styling to deal button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,15 @@ export default function Home(){
 
             <div className="dealContainer">
                 <h2>Our Most Popular Deals</h2>
+                <div className="deals">
+                    <div className="deal">
+                        <img className="dealImg" src={banner} alt="" />
+                    </div>
+                    <div className="deal">
+                        <img className="dealImg" src={banner} alt="" />
+                    </div>
+                </div>
+                <button className="dealBtn">View all deals</button>
             </div>
         </div>
     )
