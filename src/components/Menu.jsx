05bc5f71b17f2fb5f2639b6eb278441389e17156
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-05-24 12:03:11 -0600

    feat: create menu component, create route for it as well

---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Menu(){
+
+    return(
+        <div className="">
+            <h1>Menu</h1>
+        </div>
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,15 @@
 import App from "./App";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import Menu from "./components/Menu";
 const routes = [
     {
         path: "/",
         element: <App />,
         children: [
             { index: true, element: <Home />},
-            { path: "contact", element: <Contact />}
+            { path: "contact", element: <Contact />},
+            { path: "menu", element: <Menu />}
         ]
     },
     
