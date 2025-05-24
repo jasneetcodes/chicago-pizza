af45eb25019ed55e452f4bcda460aae5d8c93f38
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-05-24 10:58:01 -0600

    feat: create contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import CallSticky from './components/CallSticky'
+import Contact from './components/Contact'
 function App() {
   
 
@@ -10,7 +11,7 @@ function App() {
     <>
       <NavBar/>
       <CallSticky/>
-      <Home/>
+      <Contact/>
       <Footer/>
     </>
   )
---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+
+export default function Contact(){
+
+    return(
+        <div>
+            <h1>Contact Us</h1>
+            <div>
+                <div>
+                <h2>Opening Hours</h2>
+                <p>Monday - Saturday: 8am - 1am</p>
+                <p>Sunday: 8am - 1am</p>
+                </div>
+                <div>
+                <h2>Address</h2>
+                <p>111 Falconridge Dr, Calgary, AB T3J 2E9, Canada</p>
+                {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2505.1143017159056!2d-113.942686286891!3d51.10635123980291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537163b38d5f27f3%3A0x974d99eb42811359!2sChicago%20Deep%20Dish%20Pizza%20-%20Calgary&#39;s%20Original!5e0!3m2!1sen!2sca!4v1748105398946!5m2!1sen!2sca" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+                </div>
+            </div>
+        </div>
+    )
+}
