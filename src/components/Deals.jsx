361fa30af2401fc77c220896d26ba1ef27bd862a
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-05 11:09:58 -0600

    feat: style deal section for desktop view

--- src/components/Deals.jsx
+++ src/components/Deals.jsx
@@ -3,8 +3,9 @@ import banner from "../assets/main-deal.jpg"
 export default function Deals(){
 
     return(
-        <div className="deals">
+        <div className="dealsContainer">
             <h1>Deals</h1>
+            <div className="deals">
             <div className="deal">
                 <img src={banner} alt="" className="dealImg" />
                 <div className="dealContent">
@@ -22,6 +23,17 @@ export default function Deals(){
                     <p className="dealPrice">$19.99</p>
                 </div>
             </div>
+
+            <div className="deal">
+                <img src={banner} alt="" className="dealImg" />
+                <div className="dealContent">
+                    <h2 className="dealTitle">Combo Deal #1</h2>
+                    <p className="dealPara"> Includes 1 Large Pizza (Regular Crust) with any 4 Toppings of your choosing, 10 Chicken Wings and a 2L Pop</p>
+                    <p className="dealPrice">$19.99</p>
+                </div>
+            </div>
+            </div>
+
         </div>
     )
 }
