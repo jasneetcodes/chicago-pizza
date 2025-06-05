bda096fdc3856d9bdaa4680bae3db2e1123ae358
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-05 10:55:24 -0600

    feat: style the deal component for mobile view

--- src/components/Deals.jsx
+++ src/components/Deals.jsx
@@ -4,11 +4,21 @@ export default function Deals(){
 
     return(
         <div className="deals">
+            <h1>Deals</h1>
             <div className="deal">
                 <img src={banner} alt="" className="dealImg" />
                 <div className="dealContent">
                     <h2 className="dealTitle">Combo Deal #1</h2>
-                    <p className="dealPara">1 Large Pizza (Regular Crust) Any 4 Toppings 10 Chicken Wings (Free 2L Pop)</p>
+                    <p className="dealPara"> Includes 1 Large Pizza (Regular Crust) with any 4 Toppings of your choosing, 10 Chicken Wings and a 2L Pop</p>
+                    <p className="dealPrice">$19.99</p>
+                </div>
+            </div>
+
+            <div className="deal">
+                <img src={banner} alt="" className="dealImg" />
+                <div className="dealContent">
+                    <h2 className="dealTitle">Combo Deal #1</h2>
+                    <p className="dealPara"> Includes 1 Large Pizza (Regular Crust) with any 4 Toppings of your choosing, 10 Chicken Wings and a 2L Pop</p>
                     <p className="dealPrice">$19.99</p>
                 </div>
             </div>
