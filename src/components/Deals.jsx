253d308da690006e32ddc68dbefaaaff4f1fe953
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-05 10:34:42 -0600

    feat: create html structure for deal component

--- src/components/Deals.jsx
+++ src/components/Deals.jsx
@@ -1,10 +1,17 @@
-
+import banner from "../assets/main-deal.jpg"
 
 export default function Deals(){
 
     return(
-        <div>
-            <h1>Deals</h1>
+        <div className="deals">
+            <div className="deal">
+                <img src={banner} alt="" className="dealImg" />
+                <div className="dealContent">
+                    <h2 className="dealTitle">Combo Deal #1</h2>
+                    <p className="dealPara">1 Large Pizza (Regular Crust) Any 4 Toppings 10 Chicken Wings (Free 2L Pop)</p>
+                    <p className="dealPrice">$19.99</p>
+                </div>
+            </div>
         </div>
     )
 }
