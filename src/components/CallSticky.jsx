91cc9b1f725adf5bd983014262b1c90539b2e737
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-08 14:49:19 -0600

    feat: replace sticky call component return with a Link instead of a button

--- src/components/CallSticky.jsx
+++ src/components/CallSticky.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
-export default function CallSticky(){
-
-    return(
-        <>
-            <button className="callButton click"><a style={{color: 'white'}} href="tel:+14039844800"><i class="fa-solid fa-phone"></i></a></button>
-        </>
-    )
+export default function CallSticky() {
+  return (
+    <>
+      <Link className="callButton click" to="tel:+14039844800">
+        <i class="fa-solid fa-phone"></i>
+      </Link>
+    </>
+  );
 }
