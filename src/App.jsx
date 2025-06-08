5b16ab0e90f86f83dfbfd6643f2772c4bc5a78b3
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-08 15:34:31 -0600

    feat: create component to force scrolling to the top of the page when web path changes
    when user clicked view all deals, it did not render
    the deals page at the top, fixed it by creating a component
    that forces the page to scroll to the top whenever it detects a change
    in route path

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './components/Home'
 import Footer from './components/Footer'
 import CallSticky from './components/CallSticky'
 import Contact from './components/Contact'
+import ScrollToTop from "./components/ScrollToTop";
 import { Outlet } from 'react-router-dom'
 function App() {
   
 
   return (
     <>
+      <ScrollToTop />
       <NavBar/>
       <CallSticky/>
       <Outlet/>
