6984db1096df5f5595e9413342f65d53055ad9be
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-08 14:51:08 -0600

    feat: make social media links open on a new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,8 +30,8 @@ export default function Footer() {
         <div>
         <h3>Follow Us</h3>
         <div className="footerSocials">
-          <Link to="https://www.instagram.com"><i className="fa-brands fa-instagram fa-2xl"></i></Link>
-          <Link to="https://www.facebook.com"><i class="fa-brands fa-square-facebook fa-2xl"></i></Link>
+          <Link to="https://www.instagram.com" target="_blank"><i className="fa-brands fa-instagram fa-2xl"></i></Link>
+          <Link to="https://www.facebook.com"  target="_blank"><i class="fa-brands fa-square-facebook fa-2xl"></i></Link>
         </div>
       </div>
       </div>
