c6f6f0ca807f24bef62d3d042eeb034f55c3d353
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-08 14:43:12 -0600

    feat: replace home call button with a react Link router component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,29 @@
-import banner from "../assets/main-deal.jpg"
+import banner from "../assets/main-deal.jpg";
 import { Link } from "react-router-dom";
-export default function Home(){
-    
-    return(
-        <div className="homeContainer">
-            <img className= "mainDealImg" src={banner} alt="" />
-            <button className="callBtn click"><a style={{color: 'white'}} href="tel:+4039844800">Order Now <i class="fa-solid fa-phone "></i></a></button>
+export default function Home() {
+  return (
+    <div className="homeContainer">
+      <img className="mainDealImg" src={banner} alt="" />
+      <Link className="callBtn click" to="tel:+4039844800">
+        
+          Order Now <i class="fa-solid fa-phone "></i>
+        
+      </Link>
 
-            <div className="homeDealContainer">
-                <h2>Our Most Popular Deals</h2>
-                <div className="homeDeals">
-                    <div className="homeDeal">
-                        <img className="homeDealImg" src={banner} alt="" />
-                    </div>
-                    <div className="homeDeal">
-                        <img className="homeDealImg" src={banner} alt="" />
-                    </div>
-                </div>
-                <Link className="homeDealBtn click" to="deals">View all deals</Link>
-            </div>
+      <div className="homeDealContainer">
+        <h2>Our Most Popular Deals</h2>
+        <div className="homeDeals">
+          <div className="homeDeal">
+            <img className="homeDealImg" src={banner} alt="" />
+          </div>
+          <div className="homeDeal">
+            <img className="homeDealImg" src={banner} alt="" />
+          </div>
         </div>
-    )
+        <Link className="homeDealBtn click" to="deals">
+          View all deals
+        </Link>
+      </div>
+    </div>
+  );
 }
