dbedf987fe3fbe80f33d96a54d3c63afebe0a306
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-08 15:20:38 -0600

    feat: add different banner imgs in the home page for mobile vs tablet and desktop view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import banner from "../assets/main-deal.jpg";
+import banner from "../assets/home-banner.jpg";
+import smallViewBanner from "../assets/home-banner-mobile.webp"
+import deal from "../assets/main-deal.jpg"
 import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="homeContainer">
-      <img className="mainDealImg" src={banner} alt="" />
+        
+        <picture>
+        <source srcSet={banner} media ="(min-width: 768px)" />
+        <img className="mainDealImg" src={smallViewBanner} alt="" />
+        </picture>
+      
       <Link className="callBtn click" to="tel:+4039844800">
         
           Order Now <i class="fa-solid fa-phone "></i>
@@ -14,10 +21,10 @@ export default function Home() {
         <h2>Our Most Popular Deals</h2>
         <div className="homeDeals">
           <div className="homeDeal">
-            <img className="homeDealImg" src={banner} alt="" />
+            <img className="homeDealImg" src={deal} alt="" />
           </div>
           <div className="homeDeal">
-            <img className="homeDealImg" src={banner} alt="" />
+            <img className="homeDealImg" src={deal} alt="" />
           </div>
         </div>
         <Link className="homeDealBtn click" to="deals">
