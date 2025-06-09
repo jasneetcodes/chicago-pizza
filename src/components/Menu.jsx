af4778f32f76a9e3a1d08d22935f5adaedc765e3
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-09 15:29:35 -0600

    feat: add menu for mobile view

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,47 @@
+import menuOne from "../assets/menuOne-one.png";
+import menuOneTwo from "../assets/menuOne-two.png";
 
+import menuTwo from "../assets/menuTwo-one.png"
+import menuTwoTwo from "../assets/menuTwo-two.png"
 
-export default function Menu(){
-
-    return(
-        <div className="menuContainer">
-            
+import menuThree from "../assets/menuThree-one.png"
+import menuThreeTwo from "../assets/menuThree-two.png"
+import menuThreeThree from "../assets/menuThree-three.png"
+export default function Menu() {
+  return (
+    <div className="menuContainer">
+      <h1 className="sectionTitle">Menu</h1>
+      <div className="menuWrapper">
+        <div className="menu menuOne">
+          <div>
+            <img src={menuOne} alt="" />
+          </div>
+          <div>
+            <img src={menuOneTwo} alt="" />
+          </div>
         </div>
-    )
+
+        <div className="menu menuTwo">
+          <div>
+            <img src={menuTwo} alt="" />
+          </div>
+          <div>
+            <img src={menuTwoTwo} alt="" />
+          </div>
+        </div>
+
+        <div className="menu menuOne">
+          <div>
+            <img src={menuThree} alt="" />
+          </div>
+          <div>
+            <img src={menuThreeTwo} alt="" />
+          </div>
+          <div>
+            <img src={menuThreeThree} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
