82f39b227d5acc374505d2933378c6b8e6030ca0
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-07-14 12:54:08 -0600

    feat: add new google map address

--- src/components/MapEmbed.jsx
+++ src/components/MapEmbed.jsx
@@ -1,13 +1,24 @@
-
-export default function MapEmbed(){
-    return(
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2505.1143017159056!2d-113.942686286891!3d51.10635123980291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537163b38d5f27f3%3A0x974d99eb42811359!2sChicago%20Deep%20Dish%20Pizza%20-%20Calgary&#39;s%20Original!5e0!3m2!1sen!2sca!4v1748105398946!5m2!1sen!2sca" 
-         width="100%" 
-         height="400"
-         className="map" 
-         style={{border:0, paddingTop: 32}} 
-         allowfullscreen="" 
-         loading="lazy" 
-         referrerpolicy="no-referrer-when-downgrade"></iframe>
-    )
+export default function MapEmbed() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2505.1143017095137!2d-113.94268092246263!3d51.10635123992095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537163fd7cfcc859%3A0xae40181eabcfcb84!2sChicago%20Pizza%20Roma%20-%20Deep%20Dish%20Pizza!5e0!3m2!1sen!2sca!4v1752519097524!5m2!1sen!2sca"
+      width="100%"
+      height="400"
+      className="map"
+      style={{ border: 0, paddingTop: 32 }}
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
 }
+
+{/* <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2505.1143017095137!2d-113.94268092246263!3d51.10635123992095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537163fd7cfcc859%3A0xae40181eabcfcb84!2sChicago%20Pizza%20Roma%20-%20Deep%20Dish%20Pizza!5e0!3m2!1sen!2sca!4v1752519097524!5m2!1sen!2sca"
+  width="600"
+  height="450"
+  style="border:0;"
+  allowfullscreen=""
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade"
+></iframe>; */}
